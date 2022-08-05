6ee72bae70d0c83d3e5a9431ee60c4163afaca51
Nuno Crisóstomo <72739646+crisostomon@users.noreply.github.com>
2022-08-05 16:07:23 +0000

    Change to app-auth

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { App } = require("@octokit/app");
+const { createAppAuth } = require("@octokit/auth-app");
 
-try {
-  const app = new App({
-    appId: core.getInput('APP_ID'),
-    privateKey: core.getInput('APP_PEM'),
-  });
 
-  app.getInstallationOctokit(core.getInput('APP_INSTALLATION_ID')).then((res, err)=>{
-    if(err) return core.setFailed(err.message);
-    console.log(res);
-    return core.setOutput("app_token", "textoqq");
-  });
+const auth = new createAppAuth({
+  appId: core.getInput('APP_ID'),
+  privateKey: core.getInput('APP_PEM'),
+  installationId: core.getInput('APP_INSTALLATION_ID')
+});
 
-  
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-/* try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-} */
+auth.then((res, err) => {
+  if (err) return core.setFailed(err.message);
+  console.log(res);
+  return core.setOutput("app_token", res.token);
+});
