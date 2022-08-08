4a7fa385ae81791964b4742a85ceb8f4aeea8a70
Emanuel Fernandes <97025555+efernandes-ppb@users.noreply.github.com>
2022-08-08 14:18:58 +0000

    vanilla js implementation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const core = require('@actions/core');
-const { createAppAuth } = require("@octokit/auth-app");
-const { request } = require("@octokit/request");
+const https = require("node:https");
+const core = require("@actions/core");
+const jwt = require("jsonwebtoken");
+
+const appId = core.getInput("APP_ID");
+const privateKey = core.getInput("APP_PEM");
+const installationId = core.getInput("APP_INSTALLATION_ID");
+const inputTimeout = core.getInput('timeout');
 
 // Set defaul input to 30 seconds
 let timeout = 30000;
 
 // Set the timeout value if an input was received
-const inputTimeout = core.getInput('timeout');
 if (inputTimeout) {
   const value = Number(inputTimeout)
 
@@ -17,22 +21,65 @@ if (inputTimeout) {
   timeout = value * 1000;
 }
 
-const auth = new createAppAuth({
-  appId: core.getInput('APP_ID'),
-  privateKey: core.getInput('APP_PEM'),
-  installationId: core.getInput('APP_INSTALLATION_ID'),
-  // https://github.com/octokit/request.js#request
-  request: request.defaults({
-    request: {
-      timeout
-    }
-  })
-});
+const request = (options) =>
+  new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      if (res.statusCode !== 201)
+        return reject(
+          new Error(
+            `HTTP response status: ${res.statusCode} (${res.statusMessage})`
+          )
+        );
 
-auth({
-  type: "app"
-}).then((res, err) => {
+      let body = "";
+
+      res
+        .on("data", (chunk) => {
+          body += chunk;
+        })
+        .on("end", () => {
+          try {
+            const data = JSON.parse(body);
+            resolve(data);
+          } catch (error) {
+            reject(error);
+          }
+        });
+    });
+
+    req.on("error", (e) => {
+      reject(e);
+    });
+    req.end();
+  });
+
+const timeInSeconds = Math.floor(Date.now() / 1000);
+const token = jwt.sign(
+  {
+    // issued at time, 20 seconds in the past to allow for clock drift
+    iat: timeInSeconds - 20,
+    // JWT expiration time (10 minute maximum)
+    exp: timeInSeconds + 10 * 60,
+    // GitHub App's identifier
+    iss: appId,
+  },
+  privateKey,
+  { algorithm: "RS256" }
+);
+
+const options = {
+  method: "POST",
+  hostname: "api.github.com",
+  port: 443,
+  path: `app/installations/${installationId}/access_token`,
+  headers: {
+    Accept: "application/vnd.github.machine-man-preview+json",
+    Authorization: "Bearer " + token,
+  },
+  timeout
+};
+
+const req = request(options).then((res, err) => {
   if (err) return core.setFailed(err.message);
-  console.log(Buffer.from(res.token).toString('base64'))
   return core.setOutput("app_token", res.token);
-})
+});
